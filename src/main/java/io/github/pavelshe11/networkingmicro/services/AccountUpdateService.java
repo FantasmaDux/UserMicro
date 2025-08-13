db3ddc52f48c40e163dfce40c392d0c731f19d55
FantasmaDux <gdgdgdgdsd@mail.ru>
2025-08-13 22:59:05 +0300

    feat: тестовые данные для city, specialization

--- src/main/java/io/github/pavelshe11/networkingmicro/services/AccountUpdateService.java
+++ src/main/java/io/github/pavelshe11/networkingmicro/services/AccountUpdateService.java
@@ -6,11 +6,17 @@ import io.github.pavelshe11.networkingmicro.api.dto.requests.EmailUpdateRequestD
 import io.github.pavelshe11.networkingmicro.api.exceptions.AccountDeleteException;
 import io.github.pavelshe11.networkingmicro.api.exceptions.ServerAnswerException;
 import io.github.pavelshe11.networkingmicro.store.entities.AccountEntity;
+import io.github.pavelshe11.networkingmicro.store.entities.CityEntity;
 import io.github.pavelshe11.networkingmicro.store.repositories.AccountRepository;
+import io.github.pavelshe11.networkingmicro.store.repositories.CityRepository;
+import io.github.pavelshe11.networkingmicro.store.repositories.SpecializationRepository;
 import io.github.pavelshe11.networkingmicro.validators.AccountDataValidation;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -21,6 +27,9 @@ import java.util.UUID;
 public class AccountUpdateService {
     private final AccountRepository accountRepository;
     private final AccountDataValidation accountDataValidator;
+    private final CityRepository cityRepository;
+    private final SpecializationRepository specializationRepository;
+
 
     public void updateAccount(UUID accountId, Map<String, Object> updatedData) {
         List<ErrorDto> validationErrors = accountDataValidator.validateUpdateData(updatedData);
@@ -36,11 +45,43 @@ public class AccountUpdateService {
         }
 
         if (updatedData.containsKey("middleName")) {
-            account.setFirstName((String) updatedData.get("middleName"));
+            account.setMiddleName((String) updatedData.get("middleName"));
         }
 
         if (updatedData.containsKey("lastName")) {
-            account.setFirstName((String) updatedData.get("lastName"));
+            account.setLastName((String) updatedData.get("lastName"));
+        }
+
+        if (updatedData.containsKey("dateOfBirth")) {
+            long dateOfBirthTimestamp = Long.parseLong((String) updatedData.get("dateOfBirth"));
+            LocalDate dateOfBirth = Instant.ofEpochMilli(dateOfBirthTimestamp)
+                    .atZone(ZoneId.systemDefault())
+                    .toLocalDate();
+            account.setDateOfBirth(dateOfBirth);
+        }
+
+        if (updatedData.containsKey("idCity")) {
+            UUID cityId = UUID.fromString(updatedData.get("idCity").toString());
+            CityEntity city = cityRepository.findById(cityId).orElseThrow(() -> new ServerAnswerException());
+            account.setCity(city);
+        }
+
+        if (updatedData.containsKey("idSpecialisation")) {
+            UUID cityId = UUID.fromString(updatedData.get("idCity").toString());
+            CityEntity city = cityRepository.findById(cityId).orElseThrow(() -> new ServerAnswerException());
+            account.setCity(city);
+        }
+
+        if (updatedData.containsKey("isProfessor")) {
+            account.setProfessor((Boolean) updatedData.get("isProfessor"));
+        }
+
+        if (updatedData.containsKey("isConsulting")) {
+            account.setConsulting((Boolean) updatedData.get("isConsulting"));
+        }
+
+        if (updatedData.containsKey("courseNumber")) {
+            account.setCourseNumber((Short) updatedData.get("courseNumber"));
         }
 
         accountRepository.save(account);
