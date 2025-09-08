d8718270368785c145a0fbbf2f23f5c1ca47414a
FantasmaDux <gdgdgdgdsd@mail.ru>
2025-09-08 17:50:02 +0300

    feat: актуализация списка специальностей 1 декабря

--- src/main/java/io/github/pavelshe11/networkingmicro/store/repositories/InstitutionSpecialtiesRepository.java
+++ src/main/java/io/github/pavelshe11/networkingmicro/store/repositories/InstitutionSpecialtiesRepository.java
@@ -4,7 +4,9 @@ import io.github.pavelshe11.networkingmicro.store.entities.EducationalInstitutio
 import io.github.pavelshe11.networkingmicro.store.entities.InstitutionSpecialtiesEntity;
 import io.github.pavelshe11.networkingmicro.store.entities.SpecializationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface InstitutionSpecialtiesRepository extends JpaRepository<InstitutionSpecialtiesEntity, UUID> {
@@ -12,4 +14,15 @@ public interface InstitutionSpecialtiesRepository extends JpaRepository<Institut
     boolean existsByEducationalInstitutionAndSpecialization(EducationalInstitutionEntity institution, SpecializationEntity specialization);
 
     boolean existsByEducationalInstitutionIdAndSpecializationId(UUID id, UUID specializationId);
+
+    @Query(value = """
+            SELECT * FROM institution_specialties isp
+            WHERE NOT EXISTS (
+                            SELECT 1 FROM account
+                            WHERE account.specialization_id = isp.specialization_id
+                            AND account.educational_institution_id = isp.educational_institution_id
+                        )
+            """, nativeQuery = true
+    )
+    List<InstitutionSpecialtiesEntity> findAllUnusedSpecialties();
 }
