f7436b5c8281130d9ca49d5e305306912ef6bd95
FantasmaDux <gdgdgdgdsd@mail.ru>
2025-09-11 10:20:08 +0300

    feat: изменение dateOfBirth поля на null по дефолту #13

--- src/main/java/io/github/pavelshe11/networkingmicro/store/entities/AccountEntity.java
+++ src/main/java/io/github/pavelshe11/networkingmicro/store/entities/AccountEntity.java
@@ -83,7 +83,7 @@ public class AccountEntity {
 
     @Column(name = "date_of_birth")
     @Builder.Default
-    private LocalDate dateOfBirth = LocalDate.now();
+    private LocalDate dateOfBirth = null;
 
     @Column(name = "date_of_education_start")
     @Builder.Default
