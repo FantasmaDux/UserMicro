2007e75d40d0ab70f93705dd8eb083055eb86be6
FantasmaDux <gdgdgdgdsd@mail.ru>
2025-09-19 19:42:05 +0300

    feat: перенос обновления специализаций на quartz

---
+++ src/main/java/io/github/pavelshe11/networkingmicro/component/job/SpecializationCleanJob.java
@@ -0,0 +1,29 @@
+package io.github.pavelshe11.networkingmicro.component.job;
+
+import io.github.pavelshe11.networkingmicro.SpringContext;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@DisallowConcurrentExecution
+public class SpecializationCleanJob implements Job {
+
+    private static final Logger log = LoggerFactory.getLogger(SpecializationCleanJob.class);
+
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        try {
+            SpecializationCleanExecution executor = SpringContext.getBean(SpecializationCleanExecution.class);
+            executor.actualizeInstitutionSpecialties();
+        } catch (Exception e) {
+            log.error("Ошибка очистки специализаций", e);
+            throw new JobExecutionException(e);
+        }
+    }
+}
