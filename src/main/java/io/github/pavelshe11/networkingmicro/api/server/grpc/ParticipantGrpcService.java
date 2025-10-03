ca6b3b442407a47a1cae444c7d6551609663768e
FantasmaDux <gdgdgdgdsd@mail.ru>
2025-10-03 21:36:57 +0300

    feat: добавление типа участника в поиск grpc

--- src/main/java/io/github/pavelshe11/networkingmicro/api/server/grpc/ParticipantGrpcService.java
+++ src/main/java/io/github/pavelshe11/networkingmicro/api/server/grpc/ParticipantGrpcService.java
@@ -1,7 +1,7 @@
 package io.github.pavelshe11.networkingmicro.api.server.grpc;
 
 import io.github.pavelshe11.networkingmicro.grpc.ParticipantServiceGrpc;
-import io.github.pavelshe11.networkingmicro.grpc.findByRefIdProto;
+import io.github.pavelshe11.networkingmicro.grpc.findByRefIdAndParticipantTypeProto;
 import io.github.pavelshe11.networkingmicro.store.repositories.AccountRepository;
 import io.github.pavelshe11.networkingmicro.store.repositories.InitiativeRepository;
 import io.grpc.stub.StreamObserver;
@@ -17,13 +17,22 @@ public class ParticipantGrpcService extends ParticipantServiceGrpc.ParticipantSe
     private final InitiativeRepository initiativeRepository;
 
     @Override
-    public void findByRefId(findByRefIdProto.existsByRefIdRequest request, StreamObserver<findByRefIdProto.existsByRefIdResponse> responseObserver) {
-        boolean existing = accountRepository.existsById(UUID.fromString(request.getRefId())) ||
-                initiativeRepository.existsById(UUID.fromString(request.getRefId()));
+    public void findByRefIdAndParticipantType(findByRefIdAndParticipantTypeProto.existsByRefIdAndParticipantTypeRequest request, StreamObserver<findByRefIdAndParticipantTypeProto.existsByRefIdAndParticipantTypeResponse> responseObserver) {
 
-        findByRefIdProto.existsByRefIdResponse response = findByRefIdProto.existsByRefIdResponse.newBuilder()
-                .setExistingEntity(existing)
-                .build();
+        String participantType = request.getParticipantType().toLowerCase();
+
+        boolean existingInAccounts = accountRepository.existsById(UUID.fromString(request.getRefId()))
+                && participantType.equals("account");
+
+        boolean existingInInitiatives = initiativeRepository.existsById(UUID.fromString(request.getRefId()))
+                && participantType.equals("initiative");
+
+        boolean existing = existingInAccounts || existingInInitiatives;
+
+        findByRefIdAndParticipantTypeProto.existsByRefIdAndParticipantTypeResponse response =
+                findByRefIdAndParticipantTypeProto.existsByRefIdAndParticipantTypeResponse.newBuilder()
+                        .setExistingEntity(existing)
+                        .build();
 
         responseObserver.onNext(response);
         responseObserver.onCompleted();
